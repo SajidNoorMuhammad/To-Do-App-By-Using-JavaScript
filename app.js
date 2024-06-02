d57e8984f51241a5e2ce1ded6db30c314e163b52
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-06-02 20:43:45 +0500

    create if.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var list = document.getElementById("list");
 
 function todoApp(event) {
   var val = event.target;
-  // if (event.keyCode == 13) {
+  if (event.keyCode == 13) {
   //   list.innerHTML += `<li><input class= 'userinput textjs' type='text' value='${val.value}' disabled > 
   // <button class='edit'>Edit</button class='delete'><button class='delete'>Delete</button> <br>`
   //   text.value = "";
