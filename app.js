39979c7f9b382e6ce97ee2cc58841845bbe1fda5
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-06-02 20:42:43 +0500

    create function.

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var text = document.getElementById("text");
 var list = document.getElementById("list");
 
-// function todoApp(event) {
-//   var val = event.target;
-//   if (event.keyCode == 13) {
-//     list.innerHTML += `<li><input class= 'userinput textjs' type='text' value='${val.value}' disabled > 
-//   <button class='edit'>Edit</button class='delete'><button class='delete'>Delete</button> <br>`
-//     text.value = "";
-//   }
-// }
+function todoApp(event) {
+  // var val = event.target;
+  // if (event.keyCode == 13) {
+  //   list.innerHTML += `<li><input class= 'userinput textjs' type='text' value='${val.value}' disabled > 
+  // <button class='edit'>Edit</button class='delete'><button class='delete'>Delete</button> <br>`
+  //   text.value = "";
+  // }
+}
 // function onclicking() {
 //   list.innerHTML += `<li><input class= 'userinput textjs' type='text' value='${text.value}' disabled > 
 //   <button class='edit'>Edit</button class='delete'><button class='delete' onclick= "delet()">Delete</button> <br>`
