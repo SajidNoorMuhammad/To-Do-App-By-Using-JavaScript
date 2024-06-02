a1185ac270a5d81e29538aa1924403c1b390e6da
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-06-02 20:43:16 +0500

    create variable

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var text = document.getElementById("text");
 var list = document.getElementById("list");
 
 function todoApp(event) {
-  // var val = event.target;
+  var val = event.target;
   // if (event.keyCode == 13) {
   //   list.innerHTML += `<li><input class= 'userinput textjs' type='text' value='${val.value}' disabled > 
   // <button class='edit'>Edit</button class='delete'><button class='delete'>Delete</button> <br>`
