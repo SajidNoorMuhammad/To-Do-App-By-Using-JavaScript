6b8d4fbdc20e2ca06cba5e7fb2e05041549793eb
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-06-02 20:45:25 +0500

    again text = "";

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function todoApp(event) {
   if (event.keyCode == 13) {
     list.innerHTML += `<li><input class= 'userinput textjs' type='text' value='${val.value}' disabled > 
   <button class='edit'>Edit</button class='delete'><button class='delete'>Delete</button> <br>`
-  //   text.value = "";
+    text.value = "";
   }
 }
 // function onclicking() {
